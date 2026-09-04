500c14c86ac404233791808bd1568444b648deb7
Leonel Ramirez <50677216+leonelramirez@users.noreply.github.com>
2026-09-04 15:18:06 -0500

    feat: merged archivesspace-file-upload plugin

---
+++ frontend/controllers/concerns/handle_file_upload.rb
@@ -0,0 +1,139 @@
+module HandleFileUpload
+  extend ActiveSupport::Concern
+
+  ####################
+  # Copied from handle_crud(opts) in 
+  # frontend/app/controllers/application_controller.rb
+  # with minor changes to include file upload handling.
+  #
+  def handle_crud_with_file_uploads(opts)
+    begin
+      # Start with the JSONModel object provided, or an empty one if none was
+      # given.  Update it from the user's parameters
+      model = opts[:model] || JSONModel(opts[:instance])
+      obj = opts[:obj] || model.new
+
+      obj.instance_data[:find_opts] = opts[:find_opts] if opts.has_key? :find_opts
+
+
+      #### added for file upload handling
+      if opts[:instance] == :digital_object
+        object_id = :digital_object_id 
+      elsif opts[:instance] == :digital_object_component
+        object_id = :component_id
+
+        if params[opts[:instance]][object_id].empty?
+          params[opts[:instance]][object_id] = SecureRandom.uuid
+        end
+      end
+
+      params[opts[:instance]][object_id].strip!
+      if params[opts[:instance]].has_key?(:file_versions)
+        params[opts[:instance]][:file_versions].each do |k, v|
+          if v[:file_upload].is_a?(ActionDispatch::Http::UploadedFile)
+            begin
+              file_format_version = SecureRandom.uuid # use file_format_version as a control field to ensure the file is temporarily saved and available for upload to external applications (like Omeka) as part of the create/update process
+              saved_file_path = File.join(Dir.tmpdir, "#{file_format_version}_#{v[:file_upload].original_filename}")
+              File.open(saved_file_path, 'wb') do |file|
+                file.write(v[:file_upload].read)
+              end
+              params[opts[:instance]][:file_versions][k][:file_uri] = v[:file_upload].original_filename
+              params[opts[:instance]][:file_versions][k][:file_format_version] = file_format_version
+            rescue => e
+              obj.add_error("file_versions", :file_save_error)
+              instance_variable_set("@exceptions".intern, obj._exceptions)
+              return opts[:on_invalid].call
+            end
+
+          end
+        end
+      end
+      ####
+
+      # We need to retain any restricted properties from the existing object. i.e.
+      # properties that exist for the record but the user was not allowed to edit
+      unless params[:action] == 'copy'
+        if params[opts[:instance]].key?(:restricted_properties)
+          params[opts[:instance]][:restricted_properties].each do |restricted|
+            next unless obj.has_key? restricted
+
+            params[opts[:instance]][restricted] = obj[restricted].dup
+          end
+        end
+      end
+
+      # Param validations that don't have to do with the JSON validator
+      opts[:params_check].call(obj, params) if opts[:params_check]
+
+      instance = cleanup_params_for_schema(params[opts[:instance]], model.schema)
+
+      if opts[:before_hooks]
+        opts[:before_hooks].each { |hook| hook.call(instance) }
+      end
+
+      if opts[:replace] || opts[:replace].nil?
+        obj.replace(instance)
+      elsif opts[:copy]
+        obj.name = "Copy of " + obj.name
+        obj.uri = ''
+      else
+        obj.update(instance)
+      end
+
+      if opts[:required_fields]
+        opts[:required_fields].add_errors(obj)
+      end
+
+      # Make the updated object available to templates
+      instance_variable_set("@#{opts[:instance]}".intern, obj)
+
+      if not params.has_key?(:ignorewarnings) and not obj._warnings.empty?
+        # Throw the form back to the user to confirm warnings.
+        instance_variable_set("@exceptions".intern, obj._exceptions)
+        return opts[:on_invalid].call
+      end
+
+      if obj._exceptions[:errors]
+        instance_variable_set("@exceptions".intern, clean_exceptions(obj._exceptions))
+        return opts[:on_invalid].call
+      end
+
+      #### added for file upload handling
+      begin
+        result = JSONModel::HTTP.get_json("/repositories/#{session[:repo_id]}/find_by_id/#{opts[:instance].to_s}s", {
+          "#{object_id.to_s}[]": params[opts[:instance]][object_id],
+        })["#{opts[:instance].to_s}s"]
+
+        if (result.length > 0) && (obj.uri != result[0]['ref'])
+          obj.add_error(object_id.to_s, :must_be_unique)
+          instance_variable_set("@exceptions".intern, obj._exceptions)
+          return opts[:on_invalid].call
+        end
+      rescue SocketError => e
+        obj.add_error("file_versions", :file_server_error)
+        instance_variable_set("@exceptions".intern, obj._exceptions)
+        return opts[:on_invalid].call
+      end
+      ####
+
+      if opts.has_key?(:save_opts)
+        id = obj.save(opts[:save_opts])
+      elsif opts[:instance] == :user and !params['user']['password'].blank?
+        id = obj.save(:password => params['user']['password'])
+      else
+        id = obj.save
+      end
+
+      opts[:on_valid].call(id)
+    rescue ConflictException
+      instance_variable_set(:"@record_is_stale".intern, true)
+      opts[:on_invalid].call
+    rescue JSONModel::ValidationException => e
+      # Throw the form back to the user to display error messages.
+      instance_variable_set("@exceptions".intern, obj._exceptions)
+      opts[:on_invalid].call
+    end
+  end
+  #from commit 1bf3272
+
+end
---
+++ frontend/plugin_init.rb
@@ -0,0 +1,65 @@
+Rails.application.config.after_initialize do
+
+  class DigitalObjectComponentsController < ApplicationController
+    include HandleFileUpload
+    include ApplicationHelper
+
+    def create
+      handle_crud_with_file_uploads(:instance => :digital_object_component,
+                  :find_opts => find_opts,
+                  :on_invalid => ->() { return render_aspace_partial :partial => "new_inline" },
+                  :on_valid => ->(id) {
+                    # Refetch the record to ensure all sub records are resolved
+                    # (this object isn't marked as stale upon create like Archival Objects,
+                    # so need to do it manually)
+                    @digital_object_component = JSONModel(:digital_object_component).find(id, find_opts)
+                    digital_object = @digital_object_component['digital_object']['_resolved']
+                    parent = @digital_object_component['parent']? @digital_object_component['parent']['_resolved'] : false
+
+                    flash[:success] = @digital_object_component.parent ?
+                      t("digital_object_component._frontend.messages.created_with_parent", digital_object_component_display_string: clean_mixed_content(@digital_object_component.title), digital_object_title: clean_mixed_content(digital_object['title']), parent_display_string: clean_mixed_content(parent['title'])) :
+                      t("digital_object_component._frontend.messages.created", digital_object_component_display_string: clean_mixed_content(@digital_object_component.title), digital_object_title: clean_mixed_content(digital_object['title']))
+
+                    if @digital_object_component["is_slug_auto"] == false &&
+                      @digital_object_component["slug"] == nil &&
+                      params["digital_object_component"] &&
+                      params["digital_object_component"]["is_slug_auto"] == "1"
+
+                      flash[:warning] = t("slug.autogen_disabled")
+                    end
+
+                    render_aspace_partial :partial => "digital_object_components/edit_inline"
+                  })
+    end
+
+
+    def update
+      params['digital_object_component']['position'] = params['digital_object_component']['position'].to_i if params['digital_object_component']['position']
+
+      @digital_object_component = JSONModel(:digital_object_component).find(params[:id], find_opts)
+      digital_object = @digital_object_component['digital_object']['_resolved']
+      parent = @digital_object_component['parent'] ? @digital_object_component['parent']['_resolved'] : false
+
+      handle_crud_with_file_uploads(:instance => :digital_object_component,
+                  :obj => @digital_object_component,
+                  :on_invalid => ->() { return render_aspace_partial :partial => "edit_inline" },
+                  :on_valid => ->(id) {
+
+                    flash.now[:success] = parent ?
+                      t("digital_object_component._frontend.messages.updated_with_parent", digital_object_component_display_string: clean_mixed_content(@digital_object_component.title)) :
+                      t("digital_object_component._frontend.messages.updated", digital_object_component_display_string: clean_mixed_content(@digital_object_component.title))
+                    if @digital_object_component["is_slug_auto"] == false &&
+                      @digital_object_component["slug"] == nil &&
+                      params["digital_object_component"] &&
+                      params["digital_object_component"]["is_slug_auto"] == "1"
+
+                      flash.now[:warning] = t("slug.autogen_disabled")
+                    end
+
+                    render_aspace_partial :partial => "edit_inline"
+                  })
+    end
+
+  end
+  #from commit 0ff3a67
+end
